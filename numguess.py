dab2a66df33301127048876658ebd61831ad127b
SungjiCho <whtjdwl34@naver.com>
2019-11-07 15:13:37 +0900

    feat: loop for 3 times TODO: refactoring

--- numguess.py
+++ numguess.py
@@ -3,8 +3,13 @@ from random import randint
 
 answer = randint(1, 100+1)
 print(answer)
-guess = int(input('Hi, guess the num(1~100)> '))
-if answer == guess:
-  print("정답입니다!")
-else:
-  print("오답입니다!")
+print('Hi, welcome to guess game. You can do 3 times.')
+chance = 0
+while chance < 4:
+  guess = int(input('Guess the num(1~100)> '))
+  chance = chance + 1
+  if guess == answer:
+    print("Correct!")
+    break
+  else:
+    print("Wrong!")
