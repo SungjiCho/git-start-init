9078c901f513c5a51b0fe0ec8ea1981f549f5a86
SungjiCho <whtjdwl34@naver.com>
2019-11-07 15:26:44 +0900

    refactoring functions TODO: recursion

--- numguess.py
+++ numguess.py
@@ -4,12 +4,16 @@ from random import randint
 answer = randint(1, 100+1)
 print(answer)
 print('Hi, welcome to guess game. You can do 3 times.')
-chance = 0
-while chance < 4:
-  guess = int(input('Guess the num(1~100)> '))
-  chance = chance + 1
-  if guess == answer:
-    print("Correct!")
-    break
-  else:
-    print("Wrong!")
+
+def compare_guess():
+  chance = 3
+  for _ in range(chance):
+    guess = int(input('Guess the num(1~100)> '))
+    chance = chance + 1
+    if guess == answer:
+      print("Correct!")
+      break
+    else:
+      print("Wrong!")
+
+compare_guess()
